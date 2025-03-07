06c32f3d20e457e4477f039b4f27ba647303dad3
현우 <>
2025-03-07 16:14:44 +0900

    feat:login 완료했어욤

--- src/main/java/com/shop/cafe/controller/MemberController.java
+++ src/main/java/com/shop/cafe/controller/MemberController.java
@@ -13,12 +13,41 @@ import org.springframework.web.bind.annotation.RestController;
 import com.shop.cafe.dto.Member;
 import com.shop.cafe.service.MemberService;
 
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @RestController
 @CrossOrigin("http://127.0.0.1:5500/")
 public class MemberController {
 	
 	@Autowired
 	MemberService memberService;
+	@PostMapping("login")
+	public Map<String, String> login(@RequestBody Member m, HttpServletRequest request) {
+		
+		
+		Map<String,String> responseMap=new HashMap();
+		
+		
+		try {
+			m =  memberService.login(m);
+			String nickname=m.getNickname();
+			if(m!=null&&nickname!=null && !nickname.trim().equals("")) {//login ok
+				responseMap.put("nickanme",nickname);
+			}else {//login fail
+				responseMap.put("msg","다시 로그인 해주세요");
+			}
+		} catch (Exception e) {//login error			
+			e.printStackTrace();
+			responseMap.put("msg","다시 로그인 해주세요");
+		}
+		return responseMap;
+	}
+	
+	
+	
 	
 	@PostMapping("insertMember")
 	public String insertMember(@RequestBody  Member m) {
